c5c6332db6c934cae67044f8765640b11a1c66a4
Jacob Klymenko <klymenkojacob@gmail.com>
2025-05-28 20:45:14 -0700

    feat: add basic app outline and intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,47 @@
+// import Experience from "@/components/Experience";
+// import LinkWithIcon from "@/components/LinkWithIcon";
+// import Projects from "@/components/Projects";
+import Socials from "@/components/Socials";
+import { Button } from "@/components/ui/Button";
+import { FileDown } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const JACOB_BIRTH_YEAR = 2005;
+
+export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <article className="mt-8 flex flex-col gap-16 pb-16">
+      <section className="flex flex-col items-start gap-8 md:flex-row-reverse md:items-center md:justify-between">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className="rounded-lg"
+          src="/jacob.jpg"
+          alt="Photo of Jacob"
+          width={220}
+          height={220}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <div className="flex flex-col">
+          <h1 className="title text-5xl font-serif">heyy jacob here 👋</h1>
+          <p className="mt-2 font-medium">
+            {new Date().getFullYear() - JACOB_BIRTH_YEAR}
+            yo software engineer from Washington
+          </p>
+          <p className="mt-8 max-w-sm">
+            Student by day, full-stack dev by night, and musician on weekends.
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <section className="mt-8 flex items-center gap-8">
+            <Link href="/resume.pdf" target="_blank">
+              <Button variant="outline">
+                <span className="font-semibold">Resume</span>
+                <FileDown className="ml-2 size-5" />
+              </Button>
+            </Link>
+            <Socials />
+          </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </article>
   );
 }
---
+++ src/lib/schemas.ts
@@ -0,0 +1,36 @@
+import dynamicIconImports from "lucide-react/dynamicIconImports";
+import { z } from "zod";
+
+const iconLink = z.object({
+    name: z.string(),
+    href: z.string().url(),
+    icon: z.custom<keyof typeof dynamicIconImports>(),
+});
+export type IconLink = z.infer<typeof iconLink>;
+
+const project = z.object({
+    name: z.string(),
+    description: z.string(),
+    href: z.string().url().optional(),
+    image: z.string().optional(),
+    tags: z.array(z.string()),
+    links: z.array(iconLink),
+});
+export const projectSchema = z.object({ projects: z.array(project) });
+export type Project = z.infer<typeof project>;
+
+const experience = z.object({
+    name: z.string(),
+    href: z.string(),
+    title: z.string(),
+    logo: z.string(),
+    start: z.string(),
+    end: z.string().optional(),
+    description: z.array(z.string()).optional(),
+    links: z.array(iconLink).optional(),
+});
+export type Experience = z.infer<typeof experience>;
+
+export const careerSchema = z.object({ career: z.array(experience) });
+export const educationSchema = z.object({ education: z.array(experience) });
+export const socialSchema = z.object({ socials: z.array(iconLink) });
