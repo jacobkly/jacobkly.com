06dea7b07b1ec19c2b3f1f89e2aaf9138727dbf5
Jacob Klymenko <klymenkojacob@gmail.com>
2025-06-03 09:45:52 -0700

    fix: minor tab styles and port desc

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
     <TabsPrimitive.List
         ref={ref}
         className={cn(
-            "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+            "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground bg-neutral-800",
             className
         )}
         {...props}
@@ -29,12 +29,14 @@ const TabsTrigger = React.forwardRef<
     <TabsPrimitive.Trigger
         ref={ref}
         className={cn(
-            "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow",
+            "w-full rounded-md px-3 py-1 text-sm font-medium text-stone-300 transition-all",
+            "data-[state=active]:bg-black data-[state=active]:text-white data-[state=active]:shadow-md",
             className
         )}
         {...props}
     />
 ))
+
 TabsTrigger.displayName = "TabsTrigger"
 
 const TabsContent = React.forwardRef<
